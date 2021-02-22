ac6ba4d7dbdc7505e64285faf74225832c493688
Orosz Arnold- Daniel <oroszarnold12@yahoo.com>
2021-02-22 15:29:53 +0200

    modified invitation endpoint, added android platform

--- src/app/shared/invitation.service.ts
+++ src/app/shared/invitation.service.ts
@@ -1,5 +1,4 @@
-import { HttpClient } from "@angular/common/http";
-import { ThrowStmt } from "@angular/compiler";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { InvitationCreation } from "../model/invitation.creation.model";
@@ -18,11 +17,17 @@ export class InvitationService {
   }
 
   acceptInvitation(id: number): Observable<void> {
-    return this.httpClient.delete<void>(`api/invitations/${id}/accept`);
+    let httpParams = new HttpParams().set("accept", "true");
+    return this.httpClient.delete<void>(`api/invitations/${id}`, {
+      params: httpParams,
+    });
   }
 
   declineInvitation(id: number): Observable<void> {
-    return this.httpClient.delete<void>(`api/invitations/${id}/decline`);
+    let httpParams = new HttpParams().set("accept", "false");
+    return this.httpClient.delete<void>(`api/invitations/${id}`, {
+      params: httpParams,
+    });
   }
 
   saveInvitation(invitations: InvitationCreation): Observable<void> {
